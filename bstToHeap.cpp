2062731398557040fb67cdd898cff04c6e9e46e9
divyank100 <78527158+divyank100@users.noreply.github.com>
2022-12-22 19:38:42 +0530

    Add files via upload

---
+++ bstToHeap.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    // Find inorder of BST and store it in the vector 
+    // As inorder is in sorted form
+    // So exchange the data of BST by inorder vector.
+
+    // Not , BST has changed into  min HEAP!!
+}
